eb1a8fbd92d0baff40d372f1b7f8aed730849750
SpaceOne <space@wechall.net>
2015-05-01 17:24:32 +0200

    make client methods private to not accidently overwrite them

--- circuits/http/client/__init__.py
+++ circuits/http/client/__init__.py
@@ -28,25 +28,28 @@ class HTTPClient(BaseComponent):
 		if hasattr(self, "socket"):
 			return self.socket.connected
 
+	socket = None
+
 	def __init__(self, channel):
 		super(HTTPClient, self).__init__(channel=channel)
-		self.socket = TCPClient(channel=channel).register(self)
+		if self.socket is None:
+			self.socket = TCPClient(channel=channel).register(self)
 		self.state_machine = ClientStateMachine()
 
 	@handler("close")
-	def close(self):
+	def __on_close(self):
 		if self.socket.connected:
 			self.fire(close(), self.socket)
 
 	@handler("connect", priority=1)
-	def connect(self, event, host=None, port=None, secure=None):
+	def __on_connect(self, event, host=None, port=None, secure=None):
 		if not self.socket.connected:
 			self.fire(connect(host, port, secure), self.socket)
 
 		event.stop()
 
 	@handler("request")
-	def request(self, request):
+	def __on_request(self, request):
 
 		if not self.socket.connected:
 			host = request.uri.host
@@ -60,16 +63,16 @@ class HTTPClient(BaseComponent):
 		for data in composer:
 			self.fire(write(data), self.socket)
 
-		yield (yield self.wait("response"))
+		yield request
+		#yield (yield self.wait("response"))  # TODO: reimplement when we use client as argument
 
 	@handler("read")
-	def read(self, data):
+	def __on_read(self, data):
 		for response_ in self.state_machine.parse(data):
 			self.fire(response(response_))
 
 	@handler("response")
-	def response(self, response):
+	def __on_response(self, response):
 		if response.headers.get("Connection") == "close":  # TODO: ParsedResponse(response).close
 			self.fire(close(), self.socket)
 		return response
-
