fbf544aca3cba2102508d516c0aa153ca729f9e3
SpaceOne <space@wechall.net>
2015-05-10 14:14:09 +0200

    add httoop module hint

---
+++ circuits/http/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+try:
+	import httoop
+except ImportError as exc:
+	print('Missing httoop module. https://github.com/spaceone/httoop.\n')
+	raise
