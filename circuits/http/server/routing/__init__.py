3c3a55e12e1280bf1bd77a9ddffd56cc0f2eb12e
SpaceOne <space@wechall.net>
2015-05-28 20:30:13 +0200

    Add missing __init__ of http.server.routing

---
+++ circuits/http/server/routing/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from circuits.http.server.routing.router import Router
+from circuits.http.server.routing.domain import DomainRouter
