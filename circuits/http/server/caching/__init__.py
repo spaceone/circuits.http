1acf441e00a4d76f1f08508f0b2c7154be957136
SpaceOne <space@wechall.net>
2015-05-28 20:29:17 +0200

    Add missin __init__ of http.server.caching

---
+++ circuits/http/server/caching/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from circuits.http.server.caching.cache_control import CacheControl
+from circuits.http.server.caching.etag import ETag
+from circuits.http.server.caching.expires import Expires
+from circuits.http.server.caching.if_range import IfRange
+from circuits.http.server.caching.last_modified import LastModified
+from circuits.http.server.caching.vary import Vary
