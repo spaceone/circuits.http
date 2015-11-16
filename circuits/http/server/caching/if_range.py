80008786708435df6639063fb3e2509294928fe0
SpaceOne <space@wechall.net>
2015-11-16 22:47:50 +0100

    Fix If-Range requests

--- circuits/http/server/caching/if_range.py
+++ circuits/http/server/caching/if_range.py
@@ -6,14 +6,16 @@ from __future__ import unicode_literals
 from circuits import BaseComponent
 from circuits.http.utils import if_header_set, httphandler
 
-from httoop import EXPECTATION_FAILED
+from httoop import EXPECTATION_FAILED, Date, InvalidDate
 
 
 class IfRange(BaseComponent):
 
 	@httphandler('request', priority=0.69)
-	@if_header_set(('If-Range', 'Range'))
+	@if_header_set(('If-Range',))
 	def if_range(self, client):
+		if 'Range' not in client.request.headers:
+			return
 		if_range = client.request.headers.get('If-Range')
 		if 'Etag' in client.response.headers:
 			etag = client.response.headers.element('ETag')
@@ -22,7 +24,12 @@ class IfRange(BaseComponent):
 
 		if 'Last-Modified' in client.response.headers:
 			last_modified = client.response.headers.element('Last-Modified')
-			if last_modified == if_range:
-				return
+			try:
+				Date(if_range)
+			except InvalidDate:
+				pass
+			else:
+				if last_modified == if_range:
+					return
 
 		raise EXPECTATION_FAILED('If-Range not satisfiable.')
