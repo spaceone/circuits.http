dc1d440b6445588af803a757f00af2a410c338eb
SpaceOne <space@wechall.net>
2015-11-20 00:52:33 +0100

    Add missing circuits.http.wrapper.ssl

---
+++ circuits/http/wrapper/ssl.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+
+class SSL(object):
+
+	def __init__(self, cert=None, key=None, ca=None):
+		self.cert = cert
+		self.key = key
+		self.ca = ca
