8936b8a0839e5f0334adf23219b3d26e5a8f8404
spaceone <space@wechall.net>
2017-02-12 15:05:13 +0100

    Add hello world example

---
+++ examples/server/hello_world.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+from circuits.http.server.__main__ import HTTPServer
+from circuits.http.server.resource import Resource, method, Domain
+
+
+class HelloWorld(Resource):
+
+	path = '/'
+
+	@method
+	def GET(self, client):
+		return 'Hello World!'
+	GET.codec('text/plain')
+
+
+class Server(HTTPServer):
+
+	logformat = '%(h)s %(l)s %(u)s %(t)s %(s)s "%(r)s" "%(H)s" %(b)s "%(f)s" "%(a)s"'
+
+	def add_components(self):
+		super(Server, self).add_components()
+		root = Domain('localhost')
+		root += HelloWorld(channel='hello-world')
+		self.domains += root
+
+
+Server.main()
