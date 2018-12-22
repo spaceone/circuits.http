50e07d0c2ff7ea6a3a0068d0f9d5a3a26b3cae66
SpaceOne <space@wechall.net>
2018-12-23 00:41:38 +0100

    Prevent that multiple response events are fired

--- circuits/http/server/routing/router.py
+++ circuits/http/server/routing/router.py
@@ -17,11 +17,14 @@ class Router(BaseComponent):
 		if not evt.stopped:
 			client = evt.args[0]
 			channels = [c.channel for c in (client.domain, client.resource) if c is not None]
+			if client.events.request:
+				return
 			client.events.request = self.fire(request(client), *channels).event
 
 	@httphandler('request_success')
 	def _on_request_success(self, evt, result):
-		if not evt.stopped:
+		if not evt.stopped and not getattr(evt, '_request_success_foobar', None):
+			evt._request_success_foobar = True  # FIXME: it seems this handler is called twice (for each channel?)
 			client = evt.args[0]
 			self.fire(response(client), client.server.channel)
 
