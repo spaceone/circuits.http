d027e96c59f959d272ee5d301e3f5912f6d9d765
Florian Best <best@univention.de>
2019-08-23 02:24:00 +0200

    Fix PreconditionRequired

--- circuits/http/server/caching/precondition_required.py
+++ circuits/http/server/caching/precondition_required.py
@@ -4,12 +4,12 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 from circuits import BaseComponent
-from circuits.http.utils import sets_header, if_header_set, httphandler
+from circuits.http.utils import httphandler
 
 from httoop import PRECONDITION_REQUIRED
 
 
-class PreconditionRequired(BaseComponent)
+class PreconditionRequired(BaseComponent):
 
 	@httphandler('request', priority=0.71)
 	def _precondition_required(self, client):
@@ -17,7 +17,6 @@ class PreconditionRequired(BaseComponent)
 			return
 		if not client.response.precondition_required(client):
 			return
-		if not any(x in client.request.headers for x in ('If-Unmodified-Since', 'If-None-Match', 'If-Modified-Since', 'If-Match'):
+		if not any(x in client.request.headers for x in ('If-Unmodified-Since', 'If-None-Match', 'If-Modified-Since', 'If-Match')):
 			# TODO: evaluate the request.method
 			raise PRECONDITION_REQUIRED('Need a precondition like If-Unmodified-Since or If-None-Match.')
-
